18376932e38c671e870b29707b63ad6cc5955364
Dominic <git@msrd0.de>
2020-11-09 01:25:33 +0100

    rustfmt seems to prefer /** over ///

--- src/validate/validator.rs
+++ src/validate/validator.rs
@@ -1,26 +1,28 @@
 use std::convert::Infallible;
 
-/// This trait allows data of type `T` to be verified against custom criteria.
-///
-/// Example:
-///
-/// ```rust
-/// use gotham_formdata::FormData;
-///
-/// fn validate_password(password: &String) -> Result<(), &'static str> {
-/// 	if password.len() < 8 {
-/// 		return Err("Password is too short");
-/// 		}
-/// 	Ok(())
-/// 	}
-///
-/// #[derive(FormData)]
-/// struct LoginData {
-/// 	username: String,
-/// 	#[validate(validator = validate_password)]
-/// 	password: String
-/// 	}
-/// ```
+/**
+This trait allows data of type `T` to be verified against custom criteria.
+
+Example:
+
+```rust
+use gotham_formdata::FormData;
+
+fn validate_password(password: &String) -> Result<(), &'static str> {
+	if password.len() < 8 {
+		return Err("Password is too short");
+		}
+	Ok(())
+}
+
+#[derive(FormData)]
+struct LoginData {
+	username: String,
+	#[validate(validator = validate_password)]
+	password: String
+}
+```
+*/
 pub trait Validator<T> {
 	/// The error returned when validation failed.
 	type Err;

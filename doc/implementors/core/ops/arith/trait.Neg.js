19f5a7f0e516f763e9f50776b5f1215094f98f20
msrd0 <msrd0@users.noreply.github.com>
2021-05-19 12:37:08 +0000

    GitHub Pages for ccf0d71f68c77c406c12ffaf022107b356df4328

--- doc/implementors/core/ops/arith/trait.Neg.js
+++ doc/implementors/core/ops/arith/trait.Neg.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["time"] = [{"text":"impl Neg for Duration","synthetic":false,"types":[]},{"text":"impl Neg for Sign","synthetic":false,"types":[]}];
+implementors["time"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Neg.html\" title=\"trait core::ops::arith::Neg\">Neg</a> for <a class=\"struct\" href=\"time/struct.Duration.html\" title=\"struct time::Duration\">Duration</a>","synthetic":false,"types":["time::duration::Duration"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Neg.html\" title=\"trait core::ops::arith::Neg\">Neg</a> for <a class=\"enum\" href=\"time/enum.Sign.html\" title=\"enum time::Sign\">Sign</a>","synthetic":false,"types":["time::sign::Sign"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/implementors/rand_core/trait.RngCore.js
+++ doc/implementors/rand_core/trait.RngCore.js
@@ -1,10 +1,4 @@
 (function() {var implementors = {};
-implementors["rand_chacha"] = [{"text":"impl RngCore for ChaChaRng","synthetic":false,"types":[]}];
+implementors["rand_chacha"] = [{"text":"impl <a class=\"trait\" href=\"rand_core/trait.RngCore.html\" title=\"trait rand_core::RngCore\">RngCore</a> for <a class=\"struct\" href=\"rand_chacha/struct.ChaCha20Rng.html\" title=\"struct rand_chacha::ChaCha20Rng\">ChaCha20Rng</a>","synthetic":false,"types":["rand_chacha::chacha::ChaCha20Rng"]},{"text":"impl <a class=\"trait\" href=\"rand_core/trait.RngCore.html\" title=\"trait rand_core::RngCore\">RngCore</a> for <a class=\"struct\" href=\"rand_chacha/struct.ChaCha12Rng.html\" title=\"struct rand_chacha::ChaCha12Rng\">ChaCha12Rng</a>","synthetic":false,"types":["rand_chacha::chacha::ChaCha12Rng"]},{"text":"impl <a class=\"trait\" href=\"rand_core/trait.RngCore.html\" title=\"trait rand_core::RngCore\">RngCore</a> for <a class=\"struct\" href=\"rand_chacha/struct.ChaCha8Rng.html\" title=\"struct rand_chacha::ChaCha8Rng\">ChaCha8Rng</a>","synthetic":false,"types":["rand_chacha::chacha::ChaCha8Rng"]}];
 implementors["rand_core"] = [];
-implementors["rand_hc"] = [{"text":"impl RngCore for Hc128Rng","synthetic":false,"types":[]}];
-implementors["rand_isaac"] = [{"text":"impl RngCore for IsaacRng","synthetic":false,"types":[]},{"text":"impl RngCore for Isaac64Rng","synthetic":false,"types":[]}];
-implementors["rand_jitter"] = [{"text":"impl RngCore for JitterRng","synthetic":false,"types":[]}];
-implementors["rand_os"] = [{"text":"impl RngCore for OsRng","synthetic":false,"types":[]}];
-implementors["rand_pcg"] = [{"text":"impl RngCore for Lcg64Xsh32","synthetic":false,"types":[]},{"text":"impl RngCore for Mcg128Xsl64","synthetic":false,"types":[]}];
-implementors["rand_xorshift"] = [{"text":"impl RngCore for XorShiftRng","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
